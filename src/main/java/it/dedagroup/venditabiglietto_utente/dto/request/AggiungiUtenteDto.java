78752ba21251d145a7c1c297e01d7ad327d475c2
BRNSMN99H <BRNSMN99H@3J5FRV3.nco.inet>
2023-11-14 11:45:36 +0100

    MODIFICATA LA CLASSE PER IL ROLLBACKON (DataAccessException.class invece di Exception.class), MIGLIORATI MESSAGGI PER LE REGEX

--- src/main/java/it/dedagroup/venditabiglietto_utente/dto/request/AggiungiUtenteDto.java
+++ src/main/java/it/dedagroup/venditabiglietto_utente/dto/request/AggiungiUtenteDto.java
@@ -13,19 +13,25 @@ import lombok.Data;
 @Data
 public class AggiungiUtenteDto {
 
-	@NotNull(message = "Insert a value on field name")
-	@Pattern(regexp = "^[\\p{L} '-]+$", message = "Insert a valid name")
+	@NotNull(message = "Inserire un valore nel campo nome")
+	@Pattern(regexp = "^[\\p{L} '-]+$", message = "Inserire un nome valido")
 	private String nome;
-	@NotNull(message = "Insert a value on field surname")
-	@Pattern(regexp = "^[\\p{L} '-]+$", message = "Insert a valid surname")
+	@NotNull(message = "Inserire un valore nel campo cognome")
+	@Pattern(regexp = "^[\\p{L} '-]+$", message = "Inserire un cognome valido")
 	private String cognome;
-	@NotEmpty(message="Username cannot be empty")
+	@NotEmpty(message="Il campo username non può essere vuoto")
 	@Email
 	private String email;
-	@NotEmpty(message="Password cannot be empty")
-	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$")
+	@NotEmpty(message="Il campo password non  può essere vuoto")
+	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", 
+			message = "La password deve avere almeno 8 caratteri. "
+			+ "Non deve contenere spazi bianchi. "
+			+ "Deve contenere almeno una lettera maiuscola.\r\n"
+			+ "Deve contenere almeno una lettera minuscola.\r\n"
+			+ "Deve contenere almeno un numero.\r\n"
+			+ "Deve contenere almeno un carattere speciale tra @, #, $, %, ^, &, +, =.")
 	private String password;
-	@NotEmpty(message="Taxcode cannot be empty")
+	@NotEmpty(message="Il campo telefono non può essere vuoto")
 	@Pattern(regexp = "[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]")
 	private String telefono;
 	@NotBlank(message="la data di nascita non è valida")
--- src/main/java/it/dedagroup/venditabiglietto_utente/serviceimpl/UtenteServiceImpl.java
+++ src/main/java/it/dedagroup/venditabiglietto_utente/serviceimpl/UtenteServiceImpl.java
@@ -3,6 +3,7 @@ package it.dedagroup.venditabiglietto_utente.serviceimpl;
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -45,20 +46,20 @@ public class UtenteServiceImpl implements UtenteServiceDef {
 	}
 
 	@Override
-	@Transactional(rollbackOn = Exception.class)
+	@Transactional(rollbackOn = DataAccessException.class)
 	public void aggiungiUtente(Utente utente) {
 		utenteRepository.save(utente);
 		
 	}
 
 	@Override
-	@Transactional(rollbackOn = Exception.class)
+	@Transactional(rollbackOn = DataAccessException.class)
 	public Utente modificaUtente(Utente utente) {
 		return utenteRepository.save(utente);
 	}
 
 	@Override
-	@Transactional(rollbackOn = Exception.class)
+	@Transactional(rollbackOn = DataAccessException.class)
 	public Utente eliminaUtente(Utente utente) {
 		utente.setAvailable(false);
 		return utenteRepository.save(utente);
